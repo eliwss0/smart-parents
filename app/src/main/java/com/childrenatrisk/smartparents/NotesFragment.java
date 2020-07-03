4c3c273d7a4e672b1aca6dc84f78c1d1bd3a47e4
Eli Weiss <eliwss0@gmail.com>
2020-07-03 16:12:23 -0500

    Implemented basic notes saving

--- app/src/main/java/com/childrenatrisk/smartparents/NotesFragment.java
+++ app/src/main/java/com/childrenatrisk/smartparents/NotesFragment.java
@@ -1,5 +1,6 @@
 package com.childrenatrisk.smartparents;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,10 +8,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class NotesFragment extends Fragment {
 
-    public NotesFragment() {}
+    public NotesFragment() {
+    }
 
     public static NotesFragment newInstance() {
         NotesFragment fragment=new NotesFragment();
@@ -19,9 +29,9 @@ public class NotesFragment extends Fragment {
         return fragment;
     }
 
-    //TODO retain notes somehow (save to file with snackbar or toast to confirm save?)
+    //TODO load notes
     //TODO better switch between notes and other fragments
-    //TODO dismiss keyboard on navigate away
+    //TODO dismiss keyboard on outside tap or other input
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,12 +40,44 @@ public class NotesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_notes, container, false);
+        View view=inflater.inflate(R.layout.fragment_notes, container, false);
+        FloatingActionButton fabSave=view.findViewById(R.id.save_notes_fab);
+        final EditText notesField = view.findViewById(R.id.notes_input);
+        fabSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!notesField.getText().toString().isEmpty()) {
+                    try {
+                        writeNotesFile(getActivity(), notesField.getText().toString());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    if (fileExists(getActivity(),"notes.txt")) {
+                        Snackbar.make(view, "Saved notes", Snackbar.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+        return view;
     }
 
     @Override
     public void onResume() {
         super.onResume();
         ((MainActivity) getActivity()).setActionBarTitle("Notes");
+    }
+
+    public boolean fileExists(Context context, String filename) {
+        File file = context.getFileStreamPath(filename);
+        if (file != null && file.exists()) {
+            return true;
+        }
+        return false;
+    }
+
+    public void writeNotesFile(Context context,String fileContents) throws IOException {
+        String fileName="notes.txt";
+        FileOutputStream fileOut=context.openFileOutput(fileName, Context.MODE_PRIVATE);
+        fileOut.write(fileContents.getBytes());
     }
 }
